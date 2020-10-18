5ffcc7ef7e4cb6309d1ec0e2a49b03ed408dacc9
Allinity <72350560+Allinity@users.noreply.github.com>
2020-10-18 00:24:28 -0500

    Update AllinityClickerMadness.lua

--- AllinityClickerMadness.lua
+++ AllinityClickerMadness.lua
@@ -8,10 +8,6 @@ local UI = Material.Load({
     Theme = "Jester"
 })
 
-UI.Banner({
-	Text = "Welcome! Thanks for using Allinity Hub! Don't forget to vouch or like my post ;)"
-})
-
 local Page = UI.New({
     Title = "Clicker Madness"
 })
