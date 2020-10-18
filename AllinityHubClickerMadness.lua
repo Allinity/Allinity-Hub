4dadb801d2df8c0851f3dc88b2648b10ba4e29e2
Allinity <72350560+Allinity@users.noreply.github.com>
2020-10-18 12:49:11 -0500

    Update AllinityHubClickerMadness.lua

--- AllinityHubClickerMadness.lua
+++ AllinityHubClickerMadness.lua
@@ -9,16 +9,16 @@ local UI = Material.Load({
 })
 
 UI.Banner({
-	Text = "Welcome! Thanks for using Allinity Hub! Don't forget to vouch or like my post ;)"
+	Text = "Welcome! Thanks for using Allinity Hub"
 })
 
 local Page = UI.New({
-    Title = "Clicker Madness"
+    Title = "Main"
 })
 
 Page.Toggle({
     Text = "Auto Click",
-    Callback = function (value)
+    Callback = function(value)
         _G.click = value
 
         while true do
@@ -41,7 +41,7 @@ Menu = {
 
 Page.Toggle({
     Text = "Teleport Gems/Clicks To You",
-    Callback = function (value)
+    Callback = function(value)
         _G.Farm = value
 
         while true do
@@ -64,9 +64,13 @@ Menu = {
 }
 })
 
+local Page = UI.New({
+    Title = "Misc"
+})
+
 Page.Dropdown({
     Text = "Give Gamepass",
-    Callback = function (value)
+    Callback = function(value)
         game:GetService"Players".LocalPlayer.Gamepasses[value].Value = true
     end,
     Options = {"Sprint", "AutoRebirth", "ExplosiveCursors", "DoubleGems", "VIP", "Luck", "UnlimitedPets", "DoubleClicks", "TripleHatch"}
@@ -74,8 +78,54 @@ Page.Dropdown({
 
 Page.Dropdown({
     Text = "Rebirth",
-    Callback = function (value)
+    Callback = function(value)
         game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.BuyRebirths:FireServer(value)
     end,
     Options = {"1", "10", "100", "1000", "10000", "100000", "1000000", "10000000", "100000000", "1000000000", "10000000000", "100000000000"}
 })
+
+local Page = UI.New({
+    Title = 'Credits'
+})
+
+Page.Button({
+    Text = "Twink Marie UI Lib Creator V3RM",
+    Callback = function()
+        setclipboard('https://v3rmillion.net/member.php?action=profile&uid=1078854')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy UI Lib Creator's V3rm"            
+       })
+    end
+    }
+    })
+
+Page.Button({
+    Text = "Allinity Hub Creator V3rm",
+    Callback = function()
+        setclipboard('https://v3rmillion.net/member.php?action=profile&uid=1806998')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy Allinity Hub Creator's V3rm"            
+       })
+    end
+    }
+    })
+
+Page.Button({
+    Text = "Allinity Hub Creator Discord",
+    Callback = function()
+        setclipboard('Allinity#6969')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy Allinity Hub Creator's Discord"            
+       })
+    end
+    }
+    })
