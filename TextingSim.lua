56c2212c0d1a08642a9b00af9783382329548b91
Allinity <72350560+Allinity@users.noreply.github.com>
2020-10-18 20:31:11 -0500

    Update TextingSim.lua

--- TextingSim.lua
+++ TextingSim.lua
@@ -17,9 +17,8 @@ local UI = Material.Load({
 UI.Banner({
 	Text = "Welcome! Thanks for using Allinity Hub! Don't forget to vouch or like my post ;)"
 })
-
 local Page = UI.New({
-    Title = "Texting Sim"
+    Title = "Main"
 })
 
 Page.Toggle({
@@ -44,6 +43,9 @@ Menu = {
     end
 }
 })
+
+local Page = UI.New({
+    Title = 'Teleports'
 
 Page.Toggle({
     Text = "Tp Sell To You",
@@ -164,3 +166,49 @@ Page.Slider({
     Max = 300,
     Def = 50
 })
+
+local Page = UI.New({
+    Title = 'Credits'
+})
+
+Page.Button({
+    Text = "Twink Marie UI Lib Creator V3RM",
+    Callback = function()
+        setclipboard('https://v3rmillion.net/member.php?action=profile&uid=1078854')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy UI Lib Creator's V3rm"            
+       })
+    end
+    }
+    })
+
+Page.Button({
+    Text = "Allinity Hub Creator V3rm",
+    Callback = function()
+        setclipboard('https://v3rmillion.net/member.php?action=profile&uid=1806998')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy Allinity Hub Creator's V3rm"            
+       })
+    end
+    }
+    })
+
+Page.Button({
+    Text = "Allinity Hub Creator Discord",
+    Callback = function()
+        setclipboard('Allinity#6969')
+    end,
+Menu = {
+    Information = function(self)
+       UI.Banner({
+           Text = "Click To Copy Allinity Hub Creator's Discord"            
+       })
+    end
+    }
+    })
